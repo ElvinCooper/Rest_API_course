3425cffe9f41abaaf9c355fc179816fb35ccb16d
Elvin cooper <ing.elvin01cooper@gmail.com>
2024-12-18 15:05:08 -0400

    reconfigurando endpoints

--- app.py
+++ app.py
@@ -1,35 +1,60 @@
+import uuid
 from flask import Flask, request
+from db import stores, items
+from flask_smorest import abort
 
-app = Flask('__name__')
-
-stores = [{
-    "name": "test",
-    "items": [
-        {"name": "chair",
-         "price": 15.99}
-
-    ]
-}]
-
-@app.get('/store') # 127.0.0.1:500/store
-def get_store():
-    return stores
+app = Flask(__name__)
 
 
-@app.post('/store')
-def send_store():
-    request_data = request.get_json()
-    store = {"name": request_data["name"], "item": []}
-    stores.append(store)
+
+@app.get("/store")
+def get_stores():
+    return {"stores": list(stores.values())}
+
+@app.get("/item")
+def get_articulos():
+    return {"items": list(items.values())}
+
+
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()    
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
     return store, 201
 
 
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item
-    return {"message": "Store not found"}, 404
+@app.post("/item")  
+def create_item():
+    item_data = request.get_json()
+    if (
+        "precio" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(400, message="Bad request. Asegure que el precio y el su id esten incluidos en el json")
+    if item_data["store_id"] not in stores:
+        abort(404, message= "Store not found")
+    
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+        
+    return item
+        
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError: 
+        abort(404, message= "Store not found")
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items["item_id"]
+    except KeyError:
+        abort(404, message= "Store not found")
---
+++ db.py
@@ -0,0 +1,15 @@
+stores = {}
+items = {
+    1: {
+        "descripcion": "Mesa de escritorio",
+        "precio": 15.500
+    },
+    2 :{
+        "descripcion": "Monitor AOC",
+        "precio": 8.500
+    },
+    3 : {
+        "descripcion": "Teclado Mecanico",
+        "precio": 29.95
+    }
+}
