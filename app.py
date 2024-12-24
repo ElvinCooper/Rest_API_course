af7528fe652eccc1dfa5f95c4eae7e15cf13225d
Elvin Cooper <ing.elvin01cooper@gmail.com>
2024-12-24 10:47:46 -0400

    empezando con los esquemas de los blueprint

--- app.py
+++ app.py
@@ -3,26 +3,36 @@ from flask import Flask, request
 from db import stores, items
 from flask_smorest import abort
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 
 @app.get("/store")
 def get_stores():
     return {"stores": list(stores.values())}
 
-@app.get("/item")
-def get_articulos():
-    return {"items": list(items.values())}
-
 
 @app.post("/store")
 def create_store():
-    store_data = request.get_json()    
+    store_data = request.get_json()
+    if "descripcion" not in store_data:
+        abort(
+            400, 
+            message="Bad request. asegurese de que la descripcion este incluida en el json"
+        )
+    for store in stores.values():
+        if store_data["descripcion"] == store["descripcion"]:
+            abort(400, message=f"el store ya existe.")
+       
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
     return store, 201
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
 
 
 @app.post("/item")  
@@ -33,7 +43,15 @@ def create_item():
         or "precio" not in item_data           
         or "descripcion" not in item_data        
     ):
-        abort(400, message="Bad request. Asegure que el precio y el su id esten incluidos en el json")
+        abort(400, message="Bad request. Asegure que el precio y el id esten incluidos en el json"
+        )
+    for item in items.values():
+        if (
+            item_data["descripcion"] == item_data["descripcion"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"El item ya existe")
+
     if item_data["store_id"] not in stores:
         abort(404, message= "Store not found")
     
@@ -42,7 +60,41 @@ def create_item():
     items[item_id] = item
         
     return item, 201
-        
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "El item fue eliminado."}
+    except KeyError: 
+        abort(404, message= "Item not found")
+
+
+# enpoint para eliminar un store con su respectivo id
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "El store fue eliminado."}
+    except KeyError: 
+        abort(404, message= "Store not found")        
+
+
+# enpoint para actualizar un item con su respectivo id
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "precio" not in item_data or "descripcion" not in item_data:
+        abort(400, message="Bad request. Asegurese de que el precio y descripcion esten en el json")
+    
+    try:
+        item = item[item_id]
+        item |= item_id
+        return item
+    except KeyError:
+        abort(404, message="Item not found")
+
 
 @app.get("/store/<string:store_id>")
 def get_store(store_id):
@@ -57,4 +109,4 @@ def get_item(item_id):
     try:
         return items["item_id"]
     except KeyError:
-        abort(404, message= "Store not found")
+        abort(404, message= "Store not found"), 404
--- db.py
+++ db.py
@@ -1,18 +1,2 @@
 stores = {}
-items = {
-    1: {
-        "store_id": 1,
-        "descripcion": "Mesa de escritorio",
-        "precio": 15.500
-    },
-    2 :{
-        "store_id": 2,
-        "descripcion": "Monitor AOC",
-        "precio": 8.500
-    },
-    3 : {
-        "store_id": 3,
-        "descripcion": "Teclado Mecanico",
-        "precio": 29.95
-    }
-}
+items = {}
